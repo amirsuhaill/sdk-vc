b10c5e48575453fcd0121999f21492d320d68602
Amir suhail <amir.suhail22b@iiitg.ac.in>
2026-02-10 18:42:37 +0530

    Refactor live text handling with WebSocket support

--- src/live_text_osd.c
+++ src/live_text_osd.c
@@ -5,108 +5,152 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include<errno.h>
 
 #include<arpa/inet.h>
 #include<sys/socket.h>
+#include<unistd.h>
 
 #define SERVER_IP "192.168.1.192"
 #define SERVER_PORT 3000
-#define HTTP_BUF_SIZE 512
-
-static time_t last_switch_time = 0;
-static char live_text[20] = "Amir";
+#define RECONNECT_INTERVAL 5
 
 
-static int http_get_live_text(char *out,int out_len){
-	int sockfd;
-	struct sockaddr_in server_addr;
-	char send_buf[256];
-	char recv_buf[HTTP_BUF_SIZE];
-	int recv_len;
-	char *body;
+
+static int ws_sock = -1;
+static char live_text[20] = "Renu";
+
+static time_t last_reconnect_time = 0;
 
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		perror("socket");
-		return -1;
-	}
+static int websocket_connect(){
+    struct sockaddr_in server_addr;
+    char handshake[512];
+    char response[512];
+    int len;
 
-	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(SERVER_PORT);
-	inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+    ws_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (ws_sock < 0) {
+        perror("socket");
+        return -1;
+    }
 
-	if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
-		perror("connect");
-		close(sockfd);
-		return -1;
-	}
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
 
-	snprintf(send_buf, sizeof(send_buf), "GET /data HTTP/1.1\r\n" "Host: %s:%d\r\n" "Connection: close\r\n" "\r\n", SERVER_IP);
+    if(connect(ws_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        close(ws_sock);
+        ws_sock = -1;
+        return -1;
+    }
 
-	send(sockfd, send_buf, strlen(send_buf), 0);
+    snprintf(handshake, sizeof(handshake),
+             "GET / HTTP/1.1\r\n"
+             "Host: %s:%d\r\n"
+             "Upgrade: websocket\r\n"
+             "Connection: Upgrade\r\n"
+             "Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n"
+             "Sec-WebSocket-Version: 13\r\n"
+             "\r\n",
+             SERVER_IP, SERVER_PORT);
+    send(ws_sock, handshake, strlen(handshake), 0);
 
-	recv_len = recv(sockfd, recv_buf, sizeof(recv_buf) - 1, 0);
+    len = recv(ws_sock, response, sizeof(response) - 1, 0);
+    if (len <= 0) {
+        perror("handshake recv");
+        close(ws_sock);
+        ws_sock = -1;
+        return -1;
+    }
+    response[len] = '\0';
 
-	if(recv_len <= 0){
-		perror("recv");
-		close(sockfd);
-		return -1;
-	}
+    if(!strstr(response, "101")){
+        fprintf(stderr, "Handshake failed: %s\n", response);
+        close(ws_sock);
+        ws_sock = -1;
+        return -1;
+    }
 
-	recv_buf[recv_len] = '\0';
-	close(sockfd);
-
-	body = strstr(recv_buf, "\r\n\r\n");
-	if(!body){
-		fprintf(stderr, "Invalid HTTP response: no header-body separation\r\n");
-		return -1;
-	}
-
-	body += 4; // Skip the "\r\n\r\n"
-
-	strncpy(out, body, out_len - 1);
-	out[out_len - 1] = '\0';
-
-	out[strcspn(out, "\r\n")] = '\0';
-
-	return 0;
+    printf("WebSocket connected successfully\n");
+    return 0;
 }
 
-void updateLiveOsdText(uint8_t c_idx){
-	time_t now;
-	unsigned int i, j;
-	unsigned char input_ascii;
-	void *real_addr;
-	int width_acc = 0;
+static int websocket_recv_text(char *out,int out_len){
+    unsigned char hdr[2];
+    int payload_len;
+    int r;
 
-	now = time(NULL);
+    if(ws_sock < 0) return -1;
 
-	if(difftime(now, last_switch_time) >= 5){
-		
-		if(http_get_live_text(live_text, sizeof(live_text)) == 0){
-			printf("Updated live text: %s\n", live_text);
-		}else{
-			printf("Failed to get live text from server, keeping previous text: %s\n", live_text);
-		}
-		last_switch_time = now;
-	}
+    r = recv(ws_sock, hdr, 2, MSG_DONTWAIT);
 
-	memset((void *)p_canvas_attr_live[c_idx].canvas_addr, 0, LIVE_OSD_SIZE_WIDTH * LIVE_OSD_SIZE_HEIGHT * 2);
+    if(r == 0){
+        printf("WebSocket connection closed by server\n");
+        close(ws_sock);
+        ws_sock = -1;
+        return -1;
+    } 
+    else if(r < 0){
+        if(errno == EWOULDBLOCK || errno == EAGAIN){
+            return -2; // No data available
+        }
+        close(ws_sock);
+        ws_sock = -1;
+        return -1;
+    }
 
-	for(i=0;i<strlen(live_text);i++){
-		input_ascii = live_text[i] -32;
-		real_addr = (void *)p_font_ayuv + osdindex[input_ascii].image_offset;
+    payload_len = hdr[1] & 0x7F;
 
-		for(j=0;(UINT32)j < (osdindex[input_ascii].image_height);j++){
-			memcpy((void *)((p_canvas_attr_live[c_idx].canvas_addr) +
-			                (j * LIVE_OSD_SIZE_WIDTH * 2) + (width_acc * 2)),
-			       real_addr, (osdindex[input_ascii].image_width * 2));
-			real_addr = (real_addr + (osdindex[input_ascii].image_width * 2));
-		}
-		width_acc = width_acc + osdindex[input_ascii].image_width;
-	}
+    if(payload_len <= 0 || payload_len >= out_len) return -1;
 
-	MPI_updateOsdCanvas(g_handle[c_idx][3]);
+    r = recv(ws_sock, out, payload_len, 0);
+
+    if(r <= 0){
+        close(ws_sock);
+        ws_sock = -1;
+        return -1;
+    }
+
+    out[payload_len] = '\0';
+    return 0;
+}
+
+void updateLiveOsdText(uint8_t c_idx) {
+    time_t now = time(NULL);
+    unsigned i,j;
+    unsigned char input_ascii = 0;
+    void *real_addr = 0;
+    int width_acc = 0;
+
+    if(ws_sock < 0){
+        if(difftime(now, last_reconnect_time) >= RECONNECT_INTERVAL){
+            printf("Attempting to reconnect WebSocket...\n");
+            last_reconnect_time = now;
+            websocket_connect();
+        }
+    }
+
+    if(ws_sock >= 0){
+        if(websocket_recv_text(live_text, sizeof(live_text)) == 0){
+            printf("Received live text: %s\n", live_text);
+        }
+    }
+
+    memset((void *)p_canvas_attr_live[c_idx].canvas_addr, 0, LIVE_OSD_SIZE_WIDTH * LIVE_OSD_SIZE_HEIGHT * 2);
+
+    for(i = 0;i<strlen(live_text);i++){
+        input_ascii = live_text[i] -32;
+        real_addr = (void *)p_font_ayuv + osdindex[input_ascii].image_offset;
+
+        for(j = 0;(UINT32)j < (osdindex[input_ascii].image_height);j++){
+            memcpy((void *)((p_canvas_attr_live[c_idx].canvas_addr) + (j * LIVE_OSD_SIZE_WIDTH * 2) + (width_acc * 2)),
+                   real_addr, (osdindex[input_ascii].image_width * 2));
+            real_addr = (real_addr + (osdindex[input_ascii].image_width * 2));
+        }
+        width_acc += osdindex[input_ascii].image_width;
+    }
+    MPI_updateOsdCanvas(g_handle[c_idx][3]);
 }
